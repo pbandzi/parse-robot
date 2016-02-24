2ea3c58fd700c0fbae2e79ed19eb5b66149f0645
Juraj Linkes <juraj@noironetworks.com>
2016-02-24 09:15:44 +0100

    performance improvements

--- mongo_to_logstash.py
+++ mongo_to_logstash.py
@@ -5,15 +5,14 @@ import json
 import uuid
 import copy
 
-logging.basicConfig(filename='/var/log/mongo2elk.log', format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
 
-conflicting_fiels = {'_id', '_type', '_index', '_score', '_source'}
+conflicting_fields = {'_id', '_type', '_index', '_score', '_source'}
 
 
 def rename_conflicting_fields(json_obj):
     if isinstance(json_obj, dict):
         for key, value in json_obj.items():
-            if key in conflicting_fiels:
+            if key in conflicting_fields:
                 # rename
                 prefix = 'mongo' + key
                 new_key = 'mongo' + key
@@ -65,65 +64,61 @@ def split_testcases(test_result):
     :param test_result: json object
     :return:
     """
-    list_of_split_test_results = []
-    def process_lists(test_result):
-        long_lists = []
-        get_lists_from_children((test_result, 'details'), test_result['details'], long_lists)
-        remaining_lists = []
-
+    def process_lists(parent, child_key):
         # leave 0 length lists, flatten 1 length lists and process >2 length lists
-        for (parent, key) in long_lists:
-            long_list = parent[key]
-            if len(long_list) == 1:
-                # flatten
-                parent[key] = {}
-                long_list_value = long_list[0]
-                if isinstance(long_list_value, dict):
-                    for single_list_key, single_list_value in long_list_value.iteritems():
-                        parent[key][single_list_key] = single_list_value
-                else:
-                    parent[key] = long_list_value
-            elif len(long_list) > 1:
-                remaining_lists.append((parent, key))
+        processed_list = parent[child_key]
+        if len(processed_list) == 1:
+            # flatten
+            parent[child_key] = {}
+            processed_list_value = processed_list[0]
+            if isinstance(processed_list_value, dict):
+                # move one level up
+                for single_list_key, single_list_value in processed_list_value.iteritems():
+                    parent[child_key][single_list_key] = single_list_value
+            else:
+                # either a list or string
+                parent[child_key] = processed_list_value
 
-        return remaining_lists
+    def get_list_from_children(parent, child_key):
+        processed_child = parent[child_key]
+        if isinstance(processed_child, dict):
+            for key, value in processed_child.iteritems():
+                returned_list = get_list_from_children(processed_child, key)
+                if returned_list is not None:
+                    return returned_list
+        elif isinstance(processed_child, list):
+            if len(processed_child) < 2:
+                process_lists(parent, child_key)
+            else:
+                return parent, child_key
 
-    def get_lists_from_children(parent, json_obj, stored_lists):
-        if isinstance(json_obj, dict):
-            for key, value in json_obj.iteritems():
-                get_lists_from_children((json_obj, key), value, stored_lists)
-        elif isinstance(json_obj, list):
-            stored_lists.append(parent)
-            for list_child in json_obj:
-                get_lists_from_children((json_obj, json_obj.index(list_child)), list_child, stored_lists)
+    # def split_list_into_categories(list_of_dictionaries):
+    #     # return a list of lists of dictionaries, each list represents one category
+    #     categories = []
+    #     for dictionary in list_of_dictionaries:
+    #         categorized = False
+    #         for category in categories:
+    #             # if a match is found, append to category, which is a list
+    #             category_representative = category[0]
+    #             if set(category_representative.keys()) == set(dictionary.keys()):
+    #                 category.append(dictionary)
+    #                 categorized = True
+    #                 break
+    #
+    #         # if a match is not found, create a new category, which is a list
+    #         if not categorized:
+    #             categories.append([dictionary])
+    #
+    #     return categories
 
-    def split_list_into_categories(list_of_dictionaries):
-        # return a list of lists of dictionaries, each list represents one category
-        categories = []
-        for dictionary in list_of_dictionaries:
-            categorized = False
-            for category in categories:
-                # if a match is found, append to category, which is a list
-                category_representative = category[0]
-                if set(category_representative.keys()) == set(dictionary.keys()):
-                    category.append(dictionary)
-                    categorized = True
-                    break
-
-            # if a match is not found, create a new category, which is a list
-            if not categorized:
-                categories.append([dictionary])
-
-        return categories
-
-    # this list of tuples contains parents with child lists with length > 1
-    remaining_lists = process_lists(test_result)
-
-    if len(remaining_lists) == 0:
+    list_of_split_test_results = []
+    remaining_list = get_list_from_children(test_result, 'details')
+    if remaining_list is None:
+        # no lists that need splitting were found
         list_of_split_test_results.append(test_result)
-        # print json.dumps(test_result)
     else:
-        remaining_list_parent, remaining_list_key = remaining_lists.pop(0)
+        # this list of tuples contains parents with child lists with length > 1
+        (remaining_list_parent, remaining_list_key) = remaining_list
         # split entries into categories by looking at their dictionary keys
         # only one category should contain more than one entry
         # split this category and append the entries from other categories (somehow, if possible)
@@ -136,6 +131,7 @@ def split_testcases(test_result):
     return list_of_split_test_results
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='/var/log/mongo2elk.log', format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
     parser = argparse.ArgumentParser(description='Modify mongo json dump for logstash')
     parser.add_argument('input', help='Input json file to modify')
     args = parser.parse_args()
@@ -157,4 +153,4 @@ if __name__ == '__main__':
         parsed_test_results.extend(new_test_results)
 
     for parsed_test_result in parsed_test_results:
-        print json.dumps(parsed_test_result, indent=2)
+        print json.dumps(parsed_test_result)
